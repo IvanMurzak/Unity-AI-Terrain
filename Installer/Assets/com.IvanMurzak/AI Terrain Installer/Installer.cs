a6b7c7de045f0d2c43602cc93d8660099985ff46
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-06-29 20:43:29 -1000

    AI Game Developer update (#10)

--- Installer/Assets/com.IvanMurzak/AI Terrain Installer/Installer.cs
+++ Installer/Assets/com.IvanMurzak/AI Terrain Installer/Installer.cs
@@ -19,7 +19,7 @@ namespace com.IvanMurzak.Unity.MCP.Terrain.Installer
     public static partial class Installer
     {
         public const string PackageId = "com.ivanmurzak.unity.mcp.terrain";
-        public const string Version = "1.0.8";
+        public const string Version = "1.0.9";
 
         static Installer()
         {
